74e61a0ac6157c95ca04e4a5c0d81b73ca8140c4
Pedro Goulart <phgoulart@latam.stefanini.com>
2018-10-16 17:35:10 -0300

    Creating camera and service communication

--- xamCognitive/xamCognitive.Android/MainActivity.cs
+++ xamCognitive/xamCognitive.Android/MainActivity.cs
@@ -1,10 +1,6 @@
-﻿using System;
-
+﻿using Plugin.Media;
 using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace xamCognitive.Droid
@@ -12,14 +8,22 @@ namespace xamCognitive.Droid
     [Activity(Label = "xamCognitive", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override async void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
+
+            await CrossMedia.Current.Initialize();
+
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
